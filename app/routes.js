699795ae921989446fcf268ecb236836944cbd30
willemvangerwen-hippo <willem.van.gerwen@hippodigital.co.uk>
2026-02-16 10:21:41 +0000

    Adds form flow pages

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,28 @@
-//
-// For guidance on how to create routes see:
-// https://prototype-kit.service.gov.uk/docs/create-routes
-//
-
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+// Logic for Juggling Balls question
+router.post('/juggling-balls-answer', function (req, res) {
+  var balls = req.session.data['juggling-balls']
+
+  // If they select "None", send to ineligible page
+  if (balls == "None"){
+    res.redirect('/ineligible-juggling')
+  } else {
+    res.redirect('/juggling-trick')
+  }
+})
+
+// Logic for Swallow question
+router.post('/swallow-answer', function (req, res) {
+  var speed = req.session.data['swallow']
+
+  // Check if they guessed "24" (Monty Python reference)
+  if (speed == "24"){
+    res.redirect('/check-answers')
+  } else {
+    res.redirect('/ineligible-swallow')
+  }
+})
+
+module.exports = router
